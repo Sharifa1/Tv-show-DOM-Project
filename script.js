5aea502cb02aa11bab40a4c3de6b737417b8e0b1
Sharifa Ismail <105314490+Sharifa1@users.noreply.github.com>
2022-10-17 23:10:46 +0100

    solved level 100

--- script.js
+++ script.js
@@ -2,11 +2,50 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  // console.log(allEpisodes)
+  // allEpisodes.forEach(elem => console.log(elem.name))
+}
+
+const containerRoot = document.getElementById("root")
+
+function epCode(episode) {
+  let epNumber = episode.number;
+  let seasonNumber = episode.season;
+  if (epNumber <= 9) {
+    epNumber = `0${epNumber}`
+  } 
+  if (seasonNumber <= 9) {
+    seasonNumber = `0${seasonNumber}`
+  }
+  return `S${epNumber}E${seasonNumber}`;
+
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.className = "root";
+  episodeList.forEach(episode => {
+    const epContainer = document.createElement("div");
+    epContainer.className = "ep-container";
+    
+    rootElem.appendChild(epContainer);
+    let episodeName = document.createElement("h3");
+    episodeName.className = "ep-Name";
+    const epiCode = epCode(episode);
+    episodeName.innerText = `${episode.name} - ${epiCode}`;
+    epContainer.appendChild(episodeName);
+    const epImage = document.createElement("img");
+    epImage.className = "ep-image";
+    epImage.src = `${episode.image.medium}`;
+    epContainer.appendChild(epImage);
+    const epSummary = document.createElement("p");
+    epSummary.className = "ep-summary";
+    epSummary.innerHTML = episode.summary;
+    epContainer.appendChild(epSummary);
+
+    
+  })
+  // rootElem.innerHTML = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
